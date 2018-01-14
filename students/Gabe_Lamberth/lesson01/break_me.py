b6a5e1554ed62fd41aeb8a09eedd3b8402ff61ed
lambocalrissian <33407668+lambocalrissian@users.noreply.github.com>
2018-01-14 15:40:00 -0700

    Create break_me.py

---
+++ students/Gabe_Lamberth/lesson01/break_me.py
@@ -0,0 +1,18 @@
+def breakName():
+    print(varDoesNotExist)
+
+def breakType():
+    a = 6
+    b = '6'
+    c = a + b
+    return c
+
+def breakAttribute():
+    a = 45
+    a.add(23)
+    
+def breakSyntax():
+    a = 0 
+    b = 1
+    if a = b:
+        print("A equals B")
