e8dae54b35827a435ac2df3197d942a4b99280bb
AlexSkrn <askrn123@gmail.com>
2018-01-20 22:44:20 +0300

    fist draft solution

---
+++ students/alex_skrn/lesson02/fizzbuzz.py
@@ -0,0 +1,17 @@
+"""FizzBuzz exercise."""
+
+
+def fizzbuzz():
+    """Print Fizz Buzz numbers in the range from 1 to 100."""
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print('FizzBuzz')
+        elif i % 3 == 0:
+            print('Fizz')
+        elif i % 5 == 0:
+            print('Buzz')
+        else:
+            print(i)
+
+
+fizzbuzz()
