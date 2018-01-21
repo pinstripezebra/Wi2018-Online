98d5c4e940aadfb44a961227d1b3aa685c8cd3e8
Teck1 <Teck1@users.noreply.github.com>
2018-01-21 11:57:06 -0800

    Add generalized series function. Rework assert tests to add testing for generalized function.

--- students/ChrisH/lesson02/series.py
+++ students/ChrisH/lesson02/series.py
@@ -7,7 +7,7 @@
 def fibonacci(n):
     """
     Computes the nth value in the Fibonacci series.
-    :param num: n is the desired nth value in the series
+    :param n: n is the desired nth value in the series
     :return: Returns the nth value in the Fibonacci series
     """
     # Lower bounds check
@@ -41,7 +41,29 @@ def lucas(n):
 
     return lucas(n-1) + lucas(n-2)
 
-# General series function
+
+def sum_series(n, start1=0, start2=1):
+    """
+    Generalized function to compute the sum of the two previous values in a series.
+    Defaults to the Fibonacci series sum.
+    :param n: the desired nth value in the series
+    :param start1: value for first number in the series (Fib = 0, Lucas = 2)
+    :param start2: value for second number in the series (Fib = 1, Lucas = 1)
+    :return: nth value in the series based on start numbers
+    """
+    # Lower bounds check
+    if n <= 0:
+        return None
+
+    # Exit statements from recursion
+    if n == 1:
+        return start1
+    if n == 2:
+        return start2
+
+    # Note: have to provide start1, start2 on the recursive call, otherwise, the Lucas series
+    #  will become the Fibonacci in the recursion due to default parameter values
+    return sum_series(n-1, start1, start2) + sum_series(n-2, start1, start2)
 
 
 def main():
@@ -49,23 +71,32 @@ def main():
     Test functions using asserts.
     :return: None
     """
-
-    test_data_fibonacci = [
-        ( -15, None), (1, 0), (9, 21), (10, 34), (16, 610), (20, 4181), (30, 514229),
+    # Test data format:
+    #   List containing a List for each function to be tested.
+    #   Each function test data set has:
+    #       test function name to be called as the first element,
+    #       followed by data tuples (desired_n_value, expected_result)
+    test_data = [
+        [fibonacci, (-15, None), (1, 0), (9, 21), (10, 34), (16, 610), (20, 4181), (30, 514229), ],
+        [lucas, (-15, None), (1, 2), (2, 1), (3, 3), (4, 4), (5, 7), (6, 11), (7, 18), (30, 1149851), ],
     ]
 
     print("Assert tests:")
-    for v in test_data_fibonacci:
-        print(' fibonacci, given', v[0], 'expected result', v[1])
-        assert fibonacci(v[0]) ==  v[1]
+    for test in test_data:
+        series_fn = test[0]
+        for n, result in test[1:]:
+            print(series_fn.__name__, 'given', n, 'expected result', result)
+            assert series_fn(n) == result
 
-    test_data_lucas = [
-        (-15, None), (1, 2), (2, 1), (3, 3), (4, 4), (5, 7), (6, 11), (7, 18), (30, 1149851)
-    ]
-
-    for v in test_data_lucas:
-        print(' lucas, given', v[0], 'expected result', v[1])
-        assert lucas(v[0]) == v[1]
+    print("\nGeneralized function Assert tests:")
+    for test in test_data:
+        series_name = test[0].__name__
+        for n, result in test[1:]:
+            print(series_name, 'given', n, 'expected result', result)
+            if series_name == 'lucas':
+                assert sum_series(n, 2, 1) == result
+            else:
+                assert sum_series(n) == result      # Use default parameter values for Fibonacci
 
 
 if __name__ == "__main__":
