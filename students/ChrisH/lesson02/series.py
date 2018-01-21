d951cb0b469016ec8bdd4226c65b68fd3bf2d5bc
Teck1 <Teck1@users.noreply.github.com>
2018-01-21 11:07:48 -0800

    Add Lucas function and assert tests.

--- students/ChrisH/lesson02/series.py
+++ students/ChrisH/lesson02/series.py
@@ -23,7 +23,23 @@ def fibonacci(n):
     return fibonacci(n-1) + fibonacci(n-2)
 
 
-# Lucas function
+def lucas(n):
+    """
+    Computes the nth value in the Lucas series.
+    :param n: n is the desired nth value
+    :return: return nth value in Lucas series
+    """
+    # Lower bounds check
+    if n <= 0:
+        return None
+
+    # Exit statements from recursion
+    if n == 1:
+        return 2
+    if n == 2:
+        return 1
+
+    return lucas(n-1) + lucas(n-2)
 
 # General series function
 
@@ -42,6 +58,14 @@ def main():
     for v in test_data_fibonacci:
         print(' fibonacci, given', v[0], 'expected result', v[1])
         assert fibonacci(v[0]) ==  v[1]
+
+    test_data_lucas = [
+        (-15, None), (1, 2), (2, 1), (3, 3), (4, 4), (5, 7), (6, 11), (7, 18), (30, 1149851)
+    ]
+
+    for v in test_data_lucas:
+        print(' lucas, given', v[0], 'expected result', v[1])
+        assert lucas(v[0]) == v[1]
 
 
 if __name__ == "__main__":
