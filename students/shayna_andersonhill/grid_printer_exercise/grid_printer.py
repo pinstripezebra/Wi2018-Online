ceb0b9d5628abc7fc2c4d069fe61819154b8b7d3
Shayna Anderson-Hill <shaynaalice@gmail.com>
2018-01-23 14:55:28 -0800

    Naive grid before function

---
+++ students/shayna_andersonhill/grid_printer_exercise/grid_printer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+#Shayna Anderson-Hill
+#Script to write a function that draws a grid
+#01-22-2018
+
+plus = '+'
+minus = '-'
+space = ' '
+line = '|'
+
+
+print(plus, (minus+space)*4,plus, (minus+space)*4,plus)
+print(line, space*8, line, space*8, line)
+print(line, space*8, line, space*8, line)
+print(line, space*8, line, space*8, line)
+print(line, space*8, line, space*8, line)
+print(plus, (minus+space)*4,plus, (minus+space)*4,plus)
+print(line, space*8, line, space*8, line)
+print(line, space*8, line, space*8, line)
+print(line, space*8, line, space*8, line)
+print(line, space*8, line, space*8, line)
+print(plus, (minus+space)*4,plus, (minus+space)*4,plus)
