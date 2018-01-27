f345885c40f95cbc52caa18d610fd6cb526a3f7a
phong <trtphong@yahoo.com>
2018-01-26 16:53:32 -0800

    fix camelCase in function names

--- students/PhongT/lesson02/grid_printer.py
+++ students/PhongT/lesson02/grid_printer.py
@@ -5,19 +5,19 @@ Grid Printer Exercise
 from __future__ import print_function
 
 
-def printHead(numbar, size):
+def print_head(numbar, size):
     for x in range(1,size+1):
         print ('+', numbar * '- ', end = "")
     print ('+')
     return None
 
-def printBar(numbar, size):
+def print_bar(numbar, size):
     for x in range (1,size+1):
         print('|', numbar * 2 * ' ', end = "")
     print('|')
     return None
 
-def printGrid(numbar):
+def print_grid(numbar):
     """
     Print 2x2 grid
     :param n: number of grid cell bar
@@ -28,7 +28,7 @@ def printGrid(numbar):
     return None
 
 
-def printGrid2(size, numbar):
+def print_grid2(size, numbar):
     """
     Draw grid with a specified number of rows and columns
     :param size: number of row and column
@@ -46,17 +46,17 @@ def grid(size, numbar):
     :param numbar: number of grid cell bar
     :return:
     """
-    printHead(numbar, size)
+    print_head(numbar, size)
     for x in range(1, size+1):
         for y in range(1,numbar+1):
-            printBar(numbar, size)
-        printHead(numbar, size)
+            print_bar(numbar, size)
+        print_head(numbar, size)
     return None
 
 
 if __name__ == "__main__":
-    printGrid(5)
-    printGrid(3)
+    print_grid(5)
+    print_grid(3)
 
-    printGrid2(3, 4) # (three rows, three columns, and each grid cell four “units” in size)
-    printGrid2(5, 3)
+    print_grid2(3, 4) # (three rows, three columns, and each grid cell four “units” in size)
+    print_grid2(5, 3)
