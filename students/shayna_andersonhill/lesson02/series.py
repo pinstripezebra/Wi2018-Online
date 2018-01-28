be64934e1e083a34d1b4bab2c9168b33ec9e63c0
Shayna Anderson-Hill <shaynaalice@gmail.com>
2018-01-28 13:02:43 -0800

    Spacing fix, Nested for loop solution

---
+++ students/shayna_andersonhill/lesson02/series.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+#Shayna Anderson-Hill
+#Script to write compute the fibonacci and lucas series
+#01-24-2018
+
+
+
+def fibonacci(n):
+    fib_list = []
+    for i in range(0,n+1):
+        fib_list.append(i)
+    print(fib_list)
+
+fibonacci(4)
+
+
+#    The function should return the nth value in the fibonacci series.
