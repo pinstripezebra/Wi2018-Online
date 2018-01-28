60c3469dd76072ef5208aa43255aefdcd03a3144
davidluizrusso <david.luiz.russo@gmail.com>
2018-01-27 17:33:44 -0800

    Finished drawing_grids exercise

--- students/david_russo/lesson2/drawing_grids.py
+++ students/david_russo/lesson2/drawing_grids.py
@@ -3,20 +3,23 @@
 # This script draws grids in an increasingly general and abstract manner 
 
 # Part 1
-plus = '+ '
-minus = '- ' 
+def print_fixed_grid():
+	plus = '+ '
+	minus = '- ' 
+	# define distinct strings
+	full_line = '+ ' * 1 + '- ' * 4 + '+ ' + '- ' * 4 + '+'
+	sparse_line = '| ' + '  ' * 4 + '| ' + '  ' * 4 + '|'
 
-# define distinct strings
-full_line = '+ ' * 1 + '- ' * 4 + '+ ' + '- ' * 4 + '+'
-sparse_line = '| ' + '  ' * 4 + '| ' + '  ' * 4 + '|'
+	# print(full_line)
+	print(full_line)
+	for i in range(1, 5):
+		print(sparse_line)
+	print(full_line)
+	for i in range(1, 5):
+		print(sparse_line)
+	print(full_line)
 
-print(full_line)
-for i in range(1, 5):
-	print(sparse_line)
-print(full_line)
-for i in range(1, 5):
-	print(sparse_line)
-print(full_line)
+print_fixed_grid()
 
 # Part 2
 # Make it a function
@@ -33,14 +36,39 @@ def print_grid(n):
 		print(sparse_line)
 	print(full_line)
 
-#print_grid(3)
-print(int(3/2))
-print_grid(4)
-#print_grid(15)
+print_grid(3)
+print_grid(15)
 
 # Part 3
 # Make it a function with two parameters
 #def print_grid2(n, size):
+def print_grid2(n, size):
+
+	# initialize the line segments
+	dash_segment = ' ' + '- ' * size + '+'
+	pipe_segment = ' ' + '  ' * size + '|'
+
+	# initialize and grow the full line segments
+	full_line = '+'
+	for i in range(1, n+1):
+		full_line += dash_segment
+
+	# initialize and grow the sparse segments 
+	sparse_line = '|'
+	for i in range(1, n+1):
+		sparse_line += pipe_segment
+
+	# grow the square grid
+	print(full_line)
+	for i in range(1, n*(size + 1)):
+		if i % (size+1) == 0:
+			print(full_line)
+		else:
+			print(sparse_line)
+	print(full_line)
+
+print_grid2(5, 3)
+print_grid2(3, 4)
 
 
 
